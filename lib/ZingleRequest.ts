ecc58be992b2c313f9798ceb49360a3a5a3fd75d
Ross Calimlim <ross.calimlim@gmail.com>
2020-04-24 15:33:33 -0500

    Add options extractor

--- lib/ZingleRequest.ts
+++ lib/ZingleRequest.ts
@@ -3,6 +3,7 @@ import { AxiosResponse } from 'axios'
 import { ZingleRequestSpec } from './ZingleMethod'
 import ZingleResource from './ZingleResource'
 import { OPTIONS_KEYS } from './Zingle'
+import Utils from './Utils'
 
 // Request namespace
 const ZingleRequest = {
@@ -28,29 +29,74 @@ const ZingleRequest = {
     }, {})
   },
 
+  /**
+   * Pulls out the data object from the request args if there is one and returns it.
+   * Otherwise just returns an empty object.
+   *
+   * @param {object[]} requestArgs - array of method request args
+   * @returns {object}
+   */
   extractArgData: (requestArgs: any[]): Record<string, any> => {
     if (!Array.isArray(requestArgs) || !requestArgs[0] || typeof requestArgs[0] !== 'object') {
       return {}
     }
 
-    if (!utils.isOptionsHash(requestArgs[0])) {
+    if (!Utils.isOptionsHash(requestArgs[0])) {
       return requestArgs.shift()
     }
 
     const argKeys = Object.keys(requestArgs[0])
-
     const optionKeysInArgs = argKeys.filter((key) =>
       OPTIONS_KEYS.includes(key)
     )
-
     // error if the user put options in the damn data object
     if (optionKeysInArgs.length > 0 && optionKeysInArgs.length !== argKeys.length) {
       throw new Error(
         `Zingle: Data object should not contain options arguments: ${optionKeysInArgs.join(',')}`
       )
     }
-
     return {}
+  },
+
+  /**
+   * Return the options hash from a list of arguments
+   *
+   * @param {object[]} requestArgs - array of method request args
+   * @returns {object}
+   */
+  extractOptionsData: (requestArgs: any[]): Record<string, any> => {
+    const opts = {
+      auth: null,
+      headers: {},
+      settings: {
+        maxNetworkRetries: undefined,
+        timeout: undefined
+      }
+    }
+
+    if (requestArgs.length > 0) {
+      const arg = requestArgs[requestArgs.length - 1]
+      if (typeof arg === 'string') {
+        opts.auth = requestArgs.pop()
+      } else if (Utils.isOptionsHash(arg)) {
+        const params = requestArgs.pop()
+
+        const extraKeys = Object.keys(params).filter((key) => !OPTIONS_KEYS.includes(key))
+        if (extraKeys.length > 0) {
+          throw new Error(
+            `Zingle: Unrecognized option keys in options object ${extraKeys.join(', ')}`
+          )
+        }
+
+        if (Number.isInteger(params.maxNetworkRetries)) {
+          opts.settings.maxNetworkRetries = params.maxNetworkRetries
+        }
+        if (Number.isInteger(params.timeout)) {
+          opts.settings.timeout = params.timeout
+        }
+      }
+    }
+    return opts
   },
 
   // send axios request
@@ -60,7 +106,6 @@ const ZingleRequest = {
     spec: ZingleRequestSpec,
     overrideData: Record<string, string|number>
   ): Promise<AxiosResponse> => {
-    // ! The idea here is to manipulate and remove elemtns from the req args as needed.
     /**
      * first args could be
      *  - string representing resource id for retrieval/update/deletion
@@ -73,7 +118,7 @@ const ZingleRequest = {
 
     // build urlData from spec urlParams and requestArgs to pass to full url builder
     const urlData = ZingleRequest.buildUrlData(spec.urlParams, requestArgs)
-    const argData
+    const argData = ZingleRequest.extractArgData(requestArgs)
   }
 }
 
