cdd286889d7f6e1ffd6956c3e8a0217a24368376
Ross Calimlim <ross.calimlim@gmail.com>
2020-04-23 19:04:10 -0500

    Stub out method

---
+++ lib/ZingleMethod.ts
@@ -0,0 +1,22 @@
+import { ZingleResource } from './ZingleResource'
+
+// Method namespace
+const ZingleMethod = {
+
+  // creates a resource method based on a spec
+  generateMethod: (resource: ZingleResource, spec: ZingleMethodSpec): Function => {
+    // TODO: create actual method that returns prepped axios request
+    return (): null => null
+  }
+
+}
+
+export default ZingleMethod
+
+export interface ZingleMethodSpec {
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE';
+  methodType?: 'list';
+  path?: string; // resource path
+  host?: string; // host override if necessary
+  encode?: Function; // data encoding function
+}
